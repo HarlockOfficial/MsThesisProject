93fb7a10569364fce79ca19fd551504811c8e5f7
HarlockOfficial <harlockofficial.github@gmail.com>
2024-04-22 11:32:16 +0200

    [fix] start_classifier_test.py

--- start_classifier_test.py
+++ start_classifier_test.py
@@ -3,5 +3,5 @@ import sys
 import EEGClassificator.Tester
 
 if __name__ == '__main__':
-    path_to_network = sys.argv[1]
+    path_to_network = sys.argv[1:]
     EEGClassificator.Tester.main(path_to_network)
